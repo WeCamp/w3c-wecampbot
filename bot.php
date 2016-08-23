5a43676ba74a08c1b01be676db611c71e5ea160d
Ramon de la Fuente <ramon@future500.nl>
2016-08-23 23:28:19 +0200

    Add schedule command

--- bot.php
+++ bot.php
@@ -3,6 +3,7 @@
 require 'vendor/autoload.php';
 use PhpSlackBot\Bot;
 use W3C\Help;
+use W3C\Schedule;
 use W3C\SelfUpdate;
 use W3C\Stats;
 use W3C\WhatTheCommit;
@@ -14,6 +15,7 @@ $bot->setToken(getenv('SLACK_TOKEN'));
  * This is where the magic happens
  */
 $bot->loadCommand(new Help());
+$bot->loadCommand(new Schedule());
 $bot->loadCommand(new SelfUpdate());
 $bot->loadCommand(new Stats());
 $bot->loadCommand(new WhatTheCommit());
---
+++ src/WecampBot/Schedule.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace W3C;
+
+use PhpSlackBot\Command\BaseCommand;
+
+class Schedule extends BaseCommand
+{
+
+    private $schedule = [
+        'Wednesday' => [
+            ['from' => '08:30', 'to' => '10:00', 'activity' => 'Breakfast'],
+            ['from' => '12:45', 'to' => '13:00', 'activity' => 'Interteam standup'],
+            ['from' => '13:00', 'to' => '14:00', 'activity' => 'Lunch'],
+            ['from' => '19:00', 'to' => '20:00', 'activity' => 'Dinner + Gamenight kickoff'],
+            ['from' => '20:00', 'to' => 'late', 'activity' => 'Future500 Gamenight'],
+        ],
+        'Thursday' => [
+            ['from' => '08:30', 'to' => '10:00', 'activity' => 'Breakfast'],
+            ['from' => '10:00', 'to' => '19:00', 'activity' => 'Fronteers Frontend Rescue Team'],
+            ['from' => '12:45', 'to' => '13:00', 'activity' => 'Interteam standup'],
+            ['from' => '13:00', 'to' => '14:00', 'activity' => 'Lunch'],
+            ['from' => '19:00', 'to' => '20:00', 'activity' => 'Dinner'],
+        ],
+        'Friday' => [
+            ['from' => '08:30', 'to' => '10:00', 'activity' => 'Breakfast'],
+            ['from' => '10:00', 'to' => '13:00', 'activity' => 'Fronteers Frontend Rescue Team'],
+            ['from' => '12:45', 'to' => '13:00', 'activity' => 'Interteam standup'],
+            ['from' => '13:00', 'to' => '14:00', 'activity' => 'Lunch'],
+            ['from' => '13:00', 'to' => '16:00', 'activity' => 'Pragmatist Survival Game'],
+            ['from' => '19:00', 'to' => '20:00', 'activity' => 'Dinner'],
+        ],
+        'Saturday' => [
+            ['from' => '08:30', 'to' => '10:00', 'activity' => 'Breakfast'],
+            ['from' => '10:00', 'to' => '12:00', 'activity' => 'Preparing the presentations and backing the bags'],
+            ['from' => '12:00', 'to' => '13:00', 'activity' => 'Lunch'],
+            ['from' => '13:00', 'to' => '13:30', 'activity' => 'Project presentations'],
+            ['from' => '15:30', 'to' => '16:00', 'activity' => 'Closing'],
+            ['from' => '16:00', 'to' => 'the end', 'activity' => 'Departure'],
+        ],
+    ];
+
+    protected function configure()
+    {
+        $this->setName('!help');
+    }
+
+    protected function execute($message, $context)
+    {
+
+        $day = date('l');
+        if (!isset($this->schedule[$day])) {
+            $this->send($this->getCurrentChannel(), null, 'There is no schedule for today');
+            return;
+        }
+
+        $formattedSchedule = $day . "\n\n";
+
+        foreach ($this->schedule[$day] as $item) {
+            $formattedSchedule .= sprintf('- %s till %s %s', $item['from'], $item['to'], $item['activity']);
+        }
+
+        $this->send($this->getCurrentChannel(), null, $formattedSchedule);
+    }
+}
