8e850370f2d24ae5ff1ecc74843cf08fbf244291
Ramon de la Fuente <ramon@future500.nl>
2016-08-23 22:51:17 +0200

    Add Help command

--- bot.php
+++ bot.php
@@ -2,9 +2,10 @@
 
 require 'vendor/autoload.php';
 use PhpSlackBot\Bot;
-use W3C\WhatTheCommit;
+use W3C\Help;
 use W3C\SelfUpdate;
 use W3C\Stats;
+use W3C\WhatTheCommit;
 
 $bot = new Bot();
 $bot->setToken(getenv('SLACK_TOKEN'));
@@ -12,7 +13,8 @@ $bot->setToken(getenv('SLACK_TOKEN'));
 /**
  * This is where the magic happens
  */
-$bot->loadCommand(new WhatTheCommit());
+$bot->loadCommand(new Help());
 $bot->loadCommand(new SelfUpdate());
 $bot->loadCommand(new Stats());
+$bot->loadCommand(new WhatTheCommit());
 $bot->run();
---
+++ src/WecampBot/Help.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace W3C;
+
+use \PhpSlackBot\Command\BaseCommand;
+
+class Help extends BaseCommand
+{
+    protected function configure() {
+        $this->setName('!help');
+    }
+
+    protected function execute($message, $context)
+    {
+        $commands =  <<< EOC
+Known commands:
+
+- !commit (Suggests a commit message for your next commit)
+- !help (You are looking at it)
+- !stats [coach] (Updates the stats for the team with that coach)
+EOC;
+        $this->send($this->getCurrentChannel(), null, $commands);
+    }
+}
