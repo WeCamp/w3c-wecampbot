f1fab9cc2ab64ad8463c28b97ff434153e40a53a
Ramon de la Fuente <ramon@future500.nl>
2016-08-25 16:10:16 +0200

    TIL about whatthecommit.com/index.txt

--- src/WecampBot/WhatTheCommit.php
+++ src/WecampBot/WhatTheCommit.php
@@ -2,27 +2,17 @@
 
 namespace W3C;
 
-use Goutte\Client;
 use \PhpSlackBot\Command\BaseCommand;
 
 class WhatTheCommit extends BaseCommand
 {
-    /* Goutte\Client */
-    private $client;
-
-    public function __construct() {
-        $this->client = new Client();
-    }
-
     protected function configure() {
         $this->setName('!commit');
     }
 
-    protected function execute($message, $context) {
-        $crawler = $this->client->request('GET', 'http://whatthecommit.com/');
-        $commitMessage = $crawler->filter('#content > p')->text();
+    public function execute($message, $context) {
+        $commitMessage = readfile('http://whatthecommit.com/index.txt');
         $this->send($this->getCurrentChannel(), null, ">" . $commitMessage);
     }
 
 }
-
