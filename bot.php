c55c004145ab4b3a864cb5b7ae143eedd6c81573
Ramon de la Fuente <ramon@future500.nl>
2017-08-22 18:29:06 +0200

    Bot cleanup

--- bot.php
+++ bot.php
@@ -2,9 +2,7 @@
 
 require 'vendor/autoload.php';
 use PhpSlackBot\Bot;
-use W3C\Beer;
 use W3C\Help;
-use W3C\Schedule;
 use W3C\SelfUpdate;
 use W3C\Stats;
 use W3C\WhatTheCommit;
@@ -17,10 +15,6 @@ $bot->setToken(getenv('SLACK_TOKEN'));
  * This is where the magic happens
  */
 $bot->loadCommand(new Help());
-$bot->loadCommand(new Beer());
-$bot->loadCommand(new Schedule());
 $bot->loadCommand(new SelfUpdate());
-$bot->loadCommand(new Stats());
 $bot->loadCommand(new WhatTheCommit());
-$bot->loadCommand(new Yoda());
 $bot->run();
