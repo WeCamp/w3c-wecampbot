fbda8e842296fdae75dcf016f6f8b30aec47842b
Jeroen v.d. Gulik <jeroen@isset.nl>
2017-08-23 17:18:47 +0200

    Added Cat Bot
    Cat Bot Best Bot
    
    Signed-off-by: Jeroen v.d. Gulik <jeroen@isset.nl>

---
+++ src/WecampBot/Cat.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace W3C;
+
+use PhpSlackBot\Command\BaseCommand;
+
+final class Cat extends BaseCommand
+{
+    protected function configure()
+    {
+        $this->setName('!cat');
+    }
+
+    protected function execute($message, $context)
+    {
+        $catUrl = "http://thecatapi.com/api/images/get?format=src&type=gif";
+        $this->send($this->getCurrentChannel(), null, $catUrl);
+    }
+}
