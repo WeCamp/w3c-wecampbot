88d656b21d5e0853659f7291c9211b2976c23bbe
Steven de Vries <stevendevries@enrise.com>
2017-08-24 17:37:14 +0200

    Fixed mention🔔 for Ramon 💃 👏 👏

--- src/WecampBot/Ramon.php
+++ src/WecampBot/Ramon.php
@@ -15,6 +15,6 @@ final class Ramon extends BaseCommand
 
     protected function execute($message, $context)
     {
-        $this->send($this->getCurrentChannel(), null, '@f_u_e_n_t_e :dancer: :clap: :clap:');
+        $this->send($this->getCurrentChannel(), null, '<@f_u_e_n_t_e> :dancer: :clap: :clap:');
     }
 }
